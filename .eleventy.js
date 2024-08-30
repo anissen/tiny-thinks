bb3a5707652c37be7a32b9c87be3c9f4a7bfcc9a
Anders Nissen <andnis@gmail.com>
2024-08-30 22:52:37 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -529,8 +529,6 @@ module.exports = function (eleventyConfig) {
     ul: true,
     tags: ["h1", "h2", "h3", "h4", "h5", "h6"],
   });
-  eleventyConfig.addPlugin(EleventyHtmlBasePlugin);  
-
 
   eleventyConfig.addFilter("dateToZulu", function (date) {
     try {
